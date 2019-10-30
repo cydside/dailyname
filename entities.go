d238d2839fcb281325a4c57df5b54097c872960e
Danilo <cydside@gmail.com>
2019-10-30 15:22:07 +0100

    Minor fixes

--- entities.go
+++ entities.go
@@ -12,18 +12,18 @@ import (
 
 // UserReq descrive gli
 type UserReq struct {
-	Lang      string `json:"lang"`          // Language selection: Italian('it_IT') and English('en_US' default).
-	PathSep   string `json:"pathsep"`       // Path separator for the desired OS.
-	DateFrom  string `json:"datefrom"`      // The starting date in format YYYYMMDD(eg.: 2019-11-08 or 2019-11 or 2019) on missing data will be selected the first avaiable. If the final date won't be supplied the last day of the year will be the selected.
-	DateTo    string `json:"dateto"`        // The final datein format YYYYMMDD(eg.: 2020-01-16 or  2020-01 or  2020) on missing data will be selected the last avaiable without crossing year, if the starting date won't be supplied the first day of the year will be the selected.
-	Suffix    string `json:"suffix"`        // Add suffix to day folder's name.
-	Prefix    string `json:"prefix"`        // Add prefix to day folder's name.
-	LoneOrSub bool   `json:"loneorsub"`     // True: A long forlder name per day(eg: 2017-01-22); False: a forlder for the year, subfolders for the months and subfolders for the days(default).
+	Lang      string `json:"lang"`          // Language selection: Italian('it_IT') and English('en_US' default)
+	PathSep   string `json:"pathsep"`       // Path separator for the desired OS
+	DateFrom  string `json:"datefrom"`      // The starting date in format YYYY-MM-DD(eg.: 2019-11-08 or 2019-11 or 2019) on missing day or month will be selected the first avaiable. If the final date won't be supplied the last day of the year or month will be the selected
+	DateTo    string `json:"dateto"`        // The final date in format YYYY-MM-DD(eg.: 2020-01-16 or 2020-01) on missing day will be selected the last avaiable
+	Suffix    string `json:"suffix"`        // Add suffix to day folder's name
+	Prefix    string `json:"prefix"`        // Add prefix to day folder's name
+	LoneOrSub bool   `json:"loneorsub"`     // True: A long forlder name per day, eg: 2017-01-22 (default); False: a forlder for the year, subfolders for the months and subfolders for the days.
 	Duration  int    `json:"duration"`      // Duration in number of the days(1 to 366)
 	Content   string `json:"content"`       // Last names to add to daily name separated by comma
-	DoW       int    `json:"dayoftheweek"`  // Day's name added: Monday(long format), Mon(short format)
-	DoM       int    `json:"dayofthemonth"` // Month's name added: Jenuary(long format), Jen(short format)
-	DoY       bool   `json:"dayoftheyear"`  // Julian date added: 001 for jenuary first
+	DoW       int    `json:"dayoftheweek"`  // Day's name added: Monday(1=long format), Mon(0=short format)
+	DoM       int    `json:"dayofthemonth"` // Month's name added: Jenuary(1=long format), Jen(0=short format)
+	DoY       bool   `json:"dayoftheyear"`  // Julian date added: 001 to 365/366
 }
 
 //______________________________________________________________________________
